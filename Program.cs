1740e302d2288382ed8f374eeda91092a62d9835
Ethan Hurst <Ethan.Hurst@jjrichards.com.au>
2019-10-31 11:31:24 +1000

    performed average

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace gradebook
+namespace Gradebook
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+      grades.Add(56.1);
+
+      var result = 0.0;
+      foreach (double number in grades)
+      {
+        result += number;
+      }
+      result /= grades.Count;
+      Console.WriteLine($"The average grade is {result:N2}");
+
+      if (args.Length > 0)
+      {
+        Console.WriteLine($"Hello {args[0]}!");
+      }
+      else
+      {
+        Console.WriteLine("Hello!");
+      }
     }
+  }
 }
